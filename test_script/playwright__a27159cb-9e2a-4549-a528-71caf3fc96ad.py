f0a17c670677a24214808beee6ef99478d60577f
Chen Zhang <utgws@student.kit.edu>
2024-06-14 00:55:48 +0200

    Case 9:1)Geben Benutzername und Password ein, um sich anzumelden.2)Klicken Karte Button oben links.

--- test_script/playwright__a27159cb-9e2a-4549-a528-71caf3fc96ad.py
+++ test_script/playwright__a27159cb-9e2a-4549-a528-71caf3fc96ad.py
@@ -14,16 +14,16 @@ async def run(playwright: Playwright) -> None:
     await page.get_by_placeholder(" ").fill("Admin")
     await page.get_by_role("button", name="Anmelden").click()
     await page.get_by_role("link", name="Karte").click()
-    
-
-    return page, context, browser
-
+    await page.close()
 
     # ---------------------
-    
-    
+    await context.close()
+    await browser.close()
 
 
-async def modified_main() -> None:
+async def main() -> None:
     async with async_playwright() as playwright:
         await run(playwright)
+
+
+asyncio.run(main())
