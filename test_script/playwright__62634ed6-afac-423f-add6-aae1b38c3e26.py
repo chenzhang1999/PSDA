b155dcb9c05c7278407a874b52c71ccfa45dba96
Chen Zhang <utgws@student.kit.edu>
2024-06-13 18:11:46 +0200

    Case 2 finished

--- test_script/playwright__62634ed6-afac-423f-add6-aae1b38c3e26.py
+++ test_script/playwright__62634ed6-afac-423f-add6-aae1b38c3e26.py
@@ -17,16 +17,16 @@ async def run(playwright: Playwright) -> None:
     await page.get_by_label("Arbeitsmappe Arbeitsmappen").get_by_label("Mehr … (A)").click()
     await page.get_by_role("menuitem", name="Umbenennen …").click()
     await page.get_by_role("button", name="Abbrechen").click()
-    
-
-    return page, context, browser
-
+    await page.close()
 
     # ---------------------
-    
-    
+    await context.close()
+    await browser.close()
 
 
-async def modified_main() -> None:
+async def main() -> None:
     async with async_playwright() as playwright:
         await run(playwright)
+
+
+asyncio.run(main())
