8e7c2dc0202fb95210d7b4992b0506bfcc8cfadc
Chen Zhang <utgws@student.kit.edu>
2024-06-13 18:16:12 +0200

    Case 3 finished

--- test_script/playwright__b7d51890-30e9-4b3f-abb0-4e1891b62385.py
+++ test_script/playwright__b7d51890-30e9-4b3f-abb0-4e1891b62385.py
@@ -15,16 +15,16 @@ async def run(playwright: Playwright) -> None:
     await page.get_by_role("button", name="Anmelden").click()
     await page.get_by_text("Verzeichnis Zentrale Dienste").click()
     await page.get_by_role("link", name="Personal-Controlling").click()
-    
-
-    return page, context, browser
-
+    await page.close()
 
     # ---------------------
-    
-    
+    await context.close()
+    await browser.close()
 
 
-async def modified_main() -> None:
+async def main() -> None:
     async with async_playwright() as playwright:
         await run(playwright)
+
+
+asyncio.run(main())
